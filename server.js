773ab4a39357b5f64ec92081a685887dc19c88f0
Theologos Mavropoulos <rusinner88@gmail.com>
2023-08-31 16:30:12 +0300

    installed http-status-codes module

--- server.js
+++ server.js
@@ -5,14 +5,12 @@ const express = require("express");
 
 connectDb();
 
-const errorHandler = require("./middleware/errorHandler");
 const apiRoutes = require("./routes/apiRoutes");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/api", apiRoutes);
-app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
